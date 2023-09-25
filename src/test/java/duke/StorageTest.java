a75a599a5fb90c2abb90ea71c8d3ed394d0a4d55
Rubesh S <rubesh77@gmail.com>
2023-09-25 22:40:52 +0800

    Add more JUnit tests

--- src/test/java/duke/StorageTest.java
+++ src/test/java/duke/StorageTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
--- src/test/java/duke/UiTest.java
+++ src/test/java/duke/UiTest.java
@@ -1,17 +1,7 @@
 package duke;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -37,7 +27,7 @@ public class UiTest {
         Ui ui = new Ui();
         ui.tabPrinter("Hello World");
 
-        String expected = "      Hello World\n";
+        String expected = "      Hello World";
         assertEquals(expected, outContent.toString());
     }*/
 
